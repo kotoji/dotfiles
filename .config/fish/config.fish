df6994060f8f7280643ba5c99e291dc646e758db
kotoji <kotoji@kotojpn.com>
2023-06-26 20:11:02 +0900

    (fish) Change colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set -g fish_color_command 8890ff #8f7cff
+set -g fish_color_param a0b7f4 #769bcf
+set -g fish_color_operator a0b7f4 #a0b7f4
+set -g fish_color_end 98c78a #98c78a
+set -g fish_color_redirection a0b7f4
+set -g fish_color_quote 98c78a #98c78a
+set -g fish_color_error e88b8a #e88b8a
